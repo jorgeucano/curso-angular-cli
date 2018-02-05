a4198374d29b71db492844dc3a3dddbd77dac15b
Jorge Cano <jorgeucano@gmail.com>
2018-02-05 11:48:25 -0300

    add 04

---
+++ my-dream-app/src/app/my-new-module/my-new-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyNewModuleRoutingModule } from './my-new-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MyNewModuleRoutingModule
+  ],
+  declarations: []
+})
+export class MyNewModuleModule { }
